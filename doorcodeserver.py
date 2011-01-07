d5d3b83cb1bf31723bcfabf6245316a0739e1029
Beau Gunderson <beau@synapse.com>
2011-01-06 16:07:43 -0800

    Begin switch to an MQ-based implementation.

--- doorcodeserver.py
+++ doorcodeserver.py
@@ -1,8 +1,14 @@
-import socket
 import MySQLdb
-import ConfigParser
 import datetime
 import re
+import sys
+import zmq
+
+from ConfigParser import SafeConfigParser
+
+# TODO Refactor into a plugin class
+# TODO Refactor to use a higher-level configuration library
+# TODO Refactor to use SqlAlchemy
 
 def start_server():
     """ IPv4 server to receive occasional door code updates.
@@ -10,78 +16,70 @@ def start_server():
     The packet sent by client should be a simple string of the username
     """
 
-    #Provide default values
+    context = zmq.Context()
+
+    # Provide default values
     host = ''
     port = 0
+
     try:
         # Read config file
-        config = ConfigParser.ConfigParser()
+        config = SafeConfigParser()
         config.read("config.ini")
-        host = ''       # Symbolic name meaning all available interfaces
-        port = int(config.get("DOOR_CODE","port"))
+
+        uri = config.get("DOOR_CODE", "server-uri")
     except ConfigParser.Error:
         print "Could not read config file"
-        return
+
+       	sys.exit(1) 
 
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((host, port)) 
-    except IOError:
-        print "Could not open socket"
-        return
+        socket = context.socket(zmq.REP)
+        socket.bind(uri)
+    except zmq.ZMQError as e:
+        print "Could not bind to socket using %s: %s" % (uri, e)
 
-    while(1):
+        sys.exit(1)
+
+    print "zme.REP socket opened at %s" % uri
+
+    while True:
         try:
-            s.listen(1)
-            conn, addr = s.accept()
-            print 'Connected by', addr
+            msg = socket.recv_json()
 
-            # No packet should be larger than 1024 bytes in this case
-            userid = conn.recv(1024)
-            if userid:
-                print "received data"
-                process_data(userid)
+            if msg.userid:
+                print "Received data: %s" % msg
+
+                socket.send_json(process_data(msg))
             else:
-                print "received bad data, discarding"
-            #Close and wait for new connection
-            conn.close()
+                print "Received bad data, discarding"
         except IOError:
             print "Could not accept connection from client"
 
-
-def process_data(userid):
-
+def process_data(msg):
     try:
         config = ConfigParser.ConfigParser()
         config.read("config.ini")
-        
 
-        # Validate the data to be < 30 characters, and only digits and ascii characters
-        if len(userid)<=30 and re.match("^[a-z0-9A-Z_.]*$",userid):
-            dbconn = MySQLdb.connect(
-                host = config.get("DBInfo" , "host"),
-                user = config.get("DBInfo" , "user"),
-                passwd = config.get("DBInfo" , "password"),
-                db = config.get("DBInfo" , "dbname")
-                )
+        dbconn = MySQLdb.connect(
+            host = config.get("DBInfo" , "host"),
+            user = config.get("DBInfo" , "user"),
+            passwd = config.get("DBInfo" , "password"),
+            db = config.get("DBInfo" , "dbname"))
 
-            cursor = dbconn.cursor()
+        cursor = dbconn.cursor()
 
-            now = datetime.datetime.now()
-            # Assume user exists
-            cursor.execute("""UPDATE current_user_status 
-                              SET status = "IN", last_seen = %s 
-                              WHERE userid = %s 
-                           """ , (now, userid) )
+        now = datetime.datetime.now()
 
-        else:
-            #data recieved did not match our format
-            print "Rejected username" , userid
-    except ConfigParser.Error:
-        print "Could not read config file"
-    except MySQLdb.Error:
-        print "Could not access MySQL database"
+        # Assume user exists
+        cursor.execute("""UPDATE current_user_status 
+                          SET status = "IN", last_seen = %s 
+                          WHERE userid = %s 
+                       """ , (now, msg.userid))
 
+	return { 'Status': 'Success' }
+    except (ConfigParser.Error, MySQLdb.Error) as e:
+        return { 'Status': 'Failure', 'Exception': e }
 
 if __name__ == "__main__":
     start_server()
